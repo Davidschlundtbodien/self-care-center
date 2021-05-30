81333b29ac07101fdcf8b555a1f801b3d013e518
David Schlundt-Bodien <d.schlundtbodien@gmail.com>
2021-05-30 14:51:58 -0600

    Extract both list to a separate js file

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-var affirmations = ['I forgive myself and set myself free.', 'I believe I can be all that I want to be.', 'I am in the process of becoming the best version of myself.']
-var mantras = ['Don’t let yesterday take up too much of today.', 'Every day is a second chance.', 'Tell the truth and love everyone.']
-
 var affirmationList = document.querySelector('#affirmation-quotes');
 var mantraList = document.querySelector('#mantra-quotes');
 var messageResult = document.querySelector('.message-result');
